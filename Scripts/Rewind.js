664e68391feebd77f3b79e2731471dcd94b87bf7
PaoloTK <60204407+PaoloTK@users.noreply.github.com>
2021-05-30 18:03:37 +0200

    Create Rewind.js

---
+++ Scripts/Rewind.js
@@ -0,0 +1,6 @@
+// Change this value to increase or decrease the seconds the video will rewind at each press of the hotkey.
+// Default value = 10.
+timeUnit = 10;
+
+video = document.getElementsByClassName('video-stream')[0];
+video.currentTime = video.currentTime - timeUnit;
